fcbd1a4654fa4dc952c41f4183da542f80b303c9
Narek <narekbayanduryan16@gmail.com>
2023-10-15 20:51:03 +0300

    Created a template for logout.

--- jimi_jimi/account/urls.py
+++ jimi_jimi/account/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from django.contrib.auth import urls as auth_urls
 from . import views
 
 app_name = 'account'
 
 urlpatterns = [
-	path('dashboard/', views.dashboard, name='dashboard'),
-	path('', include(auth_urls)),
+	path('', include('django.contrib.auth.urls')),
+	path('', views.dashboard, name='dashboard'),
 ]
