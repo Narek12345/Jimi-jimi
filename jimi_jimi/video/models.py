d2bfbdf922dd6821db5bc2fd64a2f751e0e1148c
Narek <narekbayanduryan16@gmail.com>
2023-10-17 14:54:46 +0300

    Created a video model.

--- jimi_jimi/video/models.py
+++ jimi_jimi/video/models.py
@@ -1,2 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Video(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(length=150)
+	description = models.TextField()
+	public_data = models.DateTimeField(auto_now_add=True)
