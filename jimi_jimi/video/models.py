85e9e135ed1934418c16a0c2fa6392f16a60e4f5
Narek <narekbayanduryan16@gmail.com>
2023-11-05 16:54:14 +0300

    Video model update.

--- jimi_jimi/video/models.py
+++ jimi_jimi/video/models.py
@@ -6,6 +6,6 @@ class Video(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=150)
 	description = models.TextField()
-	url = models.URLField()
+	file = models.FileField(upload_to="video/%Y/%m/%d/")
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
--- jimi_jimi/video/views.py
+++ jimi_jimi/video/views.py
@@ -1,4 +1,6 @@
+import requests
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import Video
 from .forms import CreateVideoForm
 
@@ -7,6 +9,7 @@ def video_list(request):
 	return render(request, 'video/video_list.html', {})
 
 
+@ensure_csrf_cookie
 def add_video(request):
 	if request.method == 'POST':
 		form = CreateVideoForm(request.POST, request.FILES)
@@ -20,10 +23,17 @@ def add_video(request):
 
 			# Save new video.
 			new_video.save()
+
+			file_id = new_video.id
 			
 			# Update new video url.
-			new_video.url = f'http://127.0.0.1:8000/files/video/downloads/{new_video.id}'
+			url = f'http://mysite.com:5000/files/video/downloads/{file_id}'
+			new_video.url = url
 			new_video.save()
+
+			# Uploaded file to storage.
+			files = {'file': file}
+			resp = requests.post(url, files=files)
 	else:
 		form = CreateVideoForm()
 	return render(request, 'video/add_video.html', {'form': form})
